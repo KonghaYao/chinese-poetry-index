504dcb3dfbb916dd477edaa0ae53175e72c44a34
konghayao <3446798488@qq.com>
2022-09-14 13:16:05 +0800

    feat: 修改 python 上传路径

--- scripts/index.py
+++ scripts/index.py
@@ -1,7 +1,7 @@
 
 import meilisearch 
 import os
-client = meilisearch.Client('http://meili_chinese_poetry:7700', os.environ.get('MEILI_MASTER_KEY')) # masterKey 是密码
+client = meilisearch.Client(os.environ.get('MEILI_SERVER'), os.environ.get('MEILI_MASTER_KEY')) # masterKey 是密码
 
 # index 相当于数据库的表
 index = client.index('poetry')
